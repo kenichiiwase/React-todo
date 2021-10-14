f1c6ec86d046f5616b52f1f3a15435fda8096ab9
iwase <kiwase0807@gmail.com>
2021-10-14 21:50:51 +0900

    Typescriptへ置き換え close #1

---
+++ src/components/Form.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+
+type formProps = {
+  addTask(name:string):void
+}
+
+function Form(props:formProps) {
+  const [name, setName] = useState('');
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    if (!name.trim()) {
+      return;
+    }
+    props.addTask(name);
+    setName('');
+  }
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setName(event.target.value);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2 className="label-wrapper">
+        <label htmlFor="new-todo-input" className="label__lg">
+          Todo-App
+        </label>
+      </h2>
+
+      <input
+        type="text"
+        id="new-todo-input"
+        className="input input__lg"
+        name="text"
+        autoComplete="off"
+        value={name}
+        onChange={handleChange}
+      />
+      <button type="submit" className="btn btn__primary btn__lg">
+        Add
+      </button>
+    </form>
+  );
+}
+
+export default Form;
